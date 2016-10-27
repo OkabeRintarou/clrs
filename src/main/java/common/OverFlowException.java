39ee1e6b4c049e423973b3ede10eacdad3133355
OkabeRintarou <2573269786@qq.com>
2016-10-27 21:45:33 +0800

    common class

---
+++ src/main/java/common/OverFlowException.java
@@ -0,0 +1,7 @@
+package common;
+
+public class OverFlowException extends RuntimeException {
+    public OverFlowException(){
+
+    }
+}
---
+++ src/main/java/common/UnderFlowException.java
@@ -0,0 +1,7 @@
+package common;
+
+public class UnderFlowException extends RuntimeException{
+    public UnderFlowException(){
+
+    }
+}
